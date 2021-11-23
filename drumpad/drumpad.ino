9dc4894839da6673eb0703cab30e72317de4ebd1
daniel.popperl <danielpopperl@hotmail.com>
2021-11-23 01:14:22 -0300

    ok

--- drumpad/drumpad.ino
+++ drumpad/drumpad.ino
@@ -1,31 +1,49 @@
-//#include <MIDIUSB.h>
+#include <MIDIUSB.h>
 #include <Arduino.h>
 #include <TimerOne.h>
 
-#define SIG  A5
+#define SIG  0
+
+int potMessage = 0;
+int potMessage2 = 0;
+
+int _value = 0;
+int _value2 = 0;
+int _oldValue = 0;
+
+unsigned long timer2 = 0;
 
 
 const byte PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // prescaler
 
 
 //***********************************************************************
-int analogicValue[8];
+int analogicValue[2] = {0};
 
 //PORTB = B00010000 // port 8 digital
 //PORTB = B00100000 // port 9 digital
 //PORTB = B01000000 // port 10 digital
+//PORTB = B10000000 // port 11 digital
 
 // Valores em binario
-volatile byte ci[8][3] = {
-  {B00000000, B00000000, B00000000},
-  {B00000000, B00000000, B00010000},
-  {B00000000, B00100000, B00000000},
-  {B00000000, B00100000, B00010000},
-  {B01000000, B00000000, B00000000},
-  {B01000000, B00000000, B00010000},
-  {B01000000, B00100000, B00000000},
-  {B01000000, B00100000, B00010000},
-  // PORT 10     PORT 9     PORT 8
+byte ci[16][4] = {
+  {B00000000, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B00000000, B00010000},
+  {B00000000, B00000000, B00100000, B00000000},
+  {B00000000, B00000000, B00100000, B00010000},
+  {B00000000, B01000000, B00000000, B00000000},
+  {B00000000, B01000000, B00000000, B00010000},
+  {B00000000, B01000000, B00100000, B00000000},
+  {B00000000, B01000000, B00100000, B00010000},
+  {B10000000, B00000000, B00000000, B00000000},
+  {B10000000, B00000000, B00000000, B00010000},
+  {B10000000, B00000000, B00100000, B00000000},
+  {B10000000, B00000000, B00100000, B00010000},
+  {B10000000, B01000000, B00000000, B00000000},
+  {B10000000, B01000000, B00000000, B00010000},
+  {B10000000, B01000000, B00100000, B00000000},
+  {B10000000, B01000000, B00100000, B00010000},
+  // PORT 11   PORT 10     PORT 9     PORT 8
 };
 //***********************************************************************
 
@@ -49,28 +67,26 @@ class Pot
     void muxUpdate();
     void newValue(byte command, byte value, byte channel);
     byte getValue(int i);
-    byte getDebounceV();
     byte getCommand();
+    byte getControl();
+    byte getChannel2();
     byte getTypeP();
-    byte getMuxPin();
     byte getDebounce(int i);
     byte Pcommand;
     byte Pcontrol;
     byte Pchannel;
     byte Ptype;
     byte MUXPOTS;
+    unsigned long _debounce;
+    unsigned long _timer;
 
   private:
     byte _pin;
     byte _muxpin;
     byte _numMuxPins;
     byte _control;
-    int _value;
-    int _oldValue;
     bool _changed;
     byte _enablepin;
-    unsigned long _debounce;
-    unsigned long _timer;
 };
 //*************************************************************************
 
@@ -92,20 +108,6 @@ Mux::Mux(byte outpin_, byte numPins_, bool analog_)
 
 
 //****************************************************************************************
-Pot::Pot(byte pin, byte command, byte control, byte channel, byte type)
-{
-  _pin = pin;
-  _control = control;
-  //  _value = analogRead(_pin);
-  //  _value = _value >> 3;
-  //  _oldValue = _value << 3;
-  //  _value = _value << 3;
-  Pcommand = command;
-  Pcontrol = control;
-  Pchannel = channel;
-  Ptype = type;
-}
-
 Pot::Pot(Mux mux, byte muxpin, byte command, byte control, byte channel, byte type, unsigned long debounce, unsigned long timer)
 {
   _pin = mux.outpin;
@@ -114,11 +116,6 @@ Pot::Pot(Mux mux, byte muxpin, byte command, byte control, byte channel, byte ty
   _control = control;
   _timer = timer;
   _debounce = debounce;
-  //  muxUpdate();
-  //  _value = analogRead(_pin);
-  //  _value = _value >> 3;
-  //  _oldValue = _value << 3;
-  //  _value = _value << 3;
   Pcommand = command;
   Pcontrol = control;
   Pchannel = channel;
@@ -144,7 +141,7 @@ byte NUMBER_MUX_POTS = 2;
 //Mux NAME (OUTPUT PIN, , How Many Mux Pins?(8 or 16) , Is It Analog?);
 
 //Mux M1(10, 16, false); //Digital multiplexer on Arduino pin 10
-Mux M2(A5, 8, true); //Analog multiplexer on Arduino analog pin A5
+Mux M2(0, 16, true); //Analog multiplexer on Arduino analog pin A5
 //*******************************************************************
 
 
@@ -154,42 +151,52 @@ Mux M2(A5, 8, true); //Analog multiplexer on Arduino analog pin A5
 //** Type parameter 0=Potentiometer 1=Piezzo **
 
 Pot MUXPOTS[] = {
-  Pot(M2, 0, 0, 10, 1, 1, 300, 2000),
-  Pot(M2, 1, 1, 48, 1, 1, 300, 2000),
-  Pot(M2, 2, 1, 55, 1, 0, 0, 2000)
+  Pot(M2, 0, 0, 48, 1, 1, 200000, 100000),
+  Pot(M2, 1, 1, 10, 1, 0, 100000, 100000),
 };
 //*******************************************************************
 
 
-
 void setup() {
-  //  Serial1.begin(9600);
-  Serial1.begin(115200);
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  //  PRR1 |= 1 << PRUSB;
   //  pinMode(LED_BUILTIN, OUTPUT);
 
-  ADCSRA |= PS_128;
+  //  ADCSRA |= PS_128; //Pre scaler 128
 
-  Timer1.initialize(3000);
-  Timer1.attachInterrupt(multiplexReadPorts);
+//  Timer1.initialize(1000);
+//  Timer1.attachInterrupt(updateMuxPots);
+
+  // Set PORTS 8, 9, 10 and 11 to LOW
+  PORTB &= ~(1 << 4);
+  PORTB &= ~(1 << 5);
+  PORTB &= ~(1 << 6);
+  PORTB &= ~(1 << 7);
 }
 
 void loop() {
+//    multiplexReadPorts();
 
-  //  for (int x = 0; x <= 1; x++) {
-  //    Serial1.print("Pino ");
-  //    Serial1.print(x);
-  //    Serial1.print(" = ");
-  //    Serial1.println(analogicValue[x]);
-  //  }
-  //  Serial1.println("");
-  //  delay(50);
+    for (int x2 = 0; x2 <= 1; x2++) {
+      multiplexReadPorts(x2);
+      Serial.print("Pino ");
+      Serial.print(x2);
+      Serial.print(" = ");
+      Serial.println(analogicValue[x2] >> 3);
+    }
+    Serial.println("");
+    delay(10);
 
   //  if (NUMBER_BUTTONS != 0) updateButtons();
   //  if (NUMBER_POTS != 0) updatePots();
   //  if (NUMBER_MUX_BUTTONS != 0) updateMuxButtons();
-  if (NUMBER_MUX_POTS != 0) updateMuxPots();
-}
+  //  if (NUMBER_MUX_POTS != 0) updateMuxPots();
 
+}
 
 
 //***********************************************************************
@@ -199,28 +206,61 @@ void loop() {
 // Third parameter is the note number (48 = middle C).
 // Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
-//void noteOn(byte channel, byte pitch, byte velocity) {
-//  midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
-//  MidiUSB.sendMIDI(noteOn);
-//}
-//
-//void noteOff(byte channel, byte pitch, byte velocity) {
-//  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity };
-//  MidiUSB.sendMIDI(noteOff);
-//}
-//
-//// First parameter is the event type (0x0B = control change).
-//// Second parameter is the event type, combined with the channel.
-//// Third parameter is the control number number (0-119).
-//// Fourth parameter is the control value (0-127).
-//
-//void controlChange(byte channel, byte control, byte value) {
-//  midiEventPacket_t event = {0x0B, channel, control, value };
-//  MidiUSB.sendMIDI(event);
-//}
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity };
+  MidiUSB.sendMIDI(noteOff);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value };
+  MidiUSB.sendMIDI(event);
+}
 
 
 //***********************************************************************
+byte getValue(int z) {
+  multiplexReadPorts(z);
+  _value = analogicValue[z];
+
+  int tmp = (_oldValue - _value);
+
+  if (tmp >= 8 || tmp <= -(8)) {
+    _oldValue = _value >> 3;
+    _oldValue = _oldValue << 3;
+
+    return _value >> 3;
+  }
+
+  return 255;
+}
+
+byte getDebounce(int w) {
+  multiplexReadPorts(w);
+  _value2 = analogicValue[w] >> 3;
+
+  if ( micros() - MUXPOTS[w]._timer >= MUXPOTS[w]._debounce)
+  {
+    if ( _value2 >= 80 )
+    {
+      MUXPOTS[w]._timer = micros();
+
+      return _value2;
+    }
+  }
+
+  return 255;
+}
+
 byte Pot::getCommand() {
   return Pcommand;
 }
@@ -229,13 +269,6 @@ byte Pot::getTypeP() {
   return Ptype;
 }
 
-byte Pot::getMuxPin() {
-  return _muxpin;
-}
-
-byte Pot::getDebounceV() {
-  return _debounce;
-}
 
 //void Pot::muxUpdate()
 //{
@@ -250,88 +283,50 @@ byte Pot::getDebounceV() {
 //}
 
 void updateMuxPots() {
-  byte potmessage;
-  for (int i = 0; i < NUMBER_MUX_POTS; i = i + 1) {
+  for (int x = 0; x <= 1; x++) {
+    int gType = MUXPOTS[x].getTypeP();
 
-    //    MUXPOTS[i].muxUpdate();
+    if (MUXPOTS[x].getTypeP() == 1) {
+      potMessage = getDebounce(x);
 
-    if (MUXPOTS[i].getTypeP() == 0) {
-      potmessage = MUXPOTS[i].getValue(i);
-    }
-
-    if (MUXPOTS[i].getTypeP() == 1) {
-      potmessage = MUXPOTS[i].getDebounce(i);
-    }
-
-    //  Pot/Piezzo turn/hit
-    if (potmessage != 255) {
-      if (MUXPOTS[i].getCommand() == 0 ) {
-        Serial1.println("NOTE " + (String)i);
-        //        noteOn(MUXPOTS[i]->Pchannel, MUXPOTS[i]->Pcontrol, 127);
-        //        MidiUSB.flush();
-      }
-
-      if (MUXPOTS[i].getCommand() == 1 ) {
-        Serial1.println("CC " + (String) i);
-        //          controlChange(MUXPOTS[i]->Pchannel, MUXPOTS[i]->Pcontrol, potmessage);
-        //          MidiUSB.flush();
+      if (potMessage != 255) {
+        if (MUXPOTS[x].getCommand() == 0) { // NOTE
+          Serial.println("PIEZZO " + (String)potMessage);
+          noteOn(MUXPOTS[x].Pchannel, MUXPOTS[x].Pcontrol, 127);
+          MidiUSB.flush();
+        }
       }
     }
-  }
-}
 
-byte Pot::getValue(int i) {
-  _value = analogicValue[i];
-  //  _value = analogRead(_pin); //0b01100000
+    if (MUXPOTS[x].getTypeP() == 0) {
+      potMessage2 = getValue(x);
 
-  int tmp = (_oldValue - _value);
+      if (potMessage2 != 255) {
+        if (MUXPOTS[x].getCommand() == 1) { // CC
+          controlChange(MUXPOTS[x].Pchannel, MUXPOTS[x].Pcontrol, potMessage2);
+          MidiUSB.flush();
 
-  if (tmp >= 8 || tmp <= -(8)) {
-    _oldValue = _value >> 3; //0b00001100
-    _oldValue = _oldValue << 3; //0b01100000
-    return _value >> 3;
-  }
-
-  return 255;
-}
-
-byte Pot::getDebounce(int i) {
-  _value = analogicValue[i];
-  _value = _value >> 3;
-  //  _value = analogRead(_pin);
-
-  if ( _value > 70 )
-  {
-    if ( millis() - _timer >= _debounce)
-    {
-      _timer = millis();
-      return 1;
+          Serial.println("POT " + (String)potMessage2);
+        }
+      }
     }
+
   }
-
-  return 255;
 }
-
 
 //***********************************************************************
-void multiplexReadPorts() {
-
-  //    for(int x=0; x <= 7; x++){
-  //     for (int y = 0; y <= 2; y++){
-  //        digitalWrite (mix[y], ci[x][y]);
-  //      }
-  //      analogicValue[x] = analogRead(SIG);
-  //    }
-
-  for (int x = 0; x <= 1; x++) {
-    for (int y = 0; y <= 2; y++) {
-      PORTB = PORTB | ci[x][y];
-    }
-    analogicValue[x] = analogRead(SIG);
-
-    // Set PORTS 8, 9 and 10 to LOW
-    PORTB &= ~(1 << 4);
-    PORTB &= ~(1 << 5);
-    PORTB &= ~(1 << 6);
+void multiplexReadPorts(int por) {
+  //  for (int x = 0; x <= 1; x++) {
+  for (int y = 0; y <= 3; y++) {
+    PORTB |= ci[por][y];
   }
+
+  analogicValue[por] = analogRead(A0);
+  //  }
+
+  // Set PORTS 8, 9, 10 and 11 to LOW
+  PORTB &= ~(1 << 4);
+  PORTB &= ~(1 << 5);
+  PORTB &= ~(1 << 6);
+  PORTB &= ~(1 << 7);
 }
